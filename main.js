a5c0fce3fec170e459e72516b869100440aa9740
ISHANCODERJR <104986815+ISHANCODERJR@users.noreply.github.com>
2023-11-16 13:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    loadImage('https://i.postimg.cc/7PCvt1TX/ele.png')
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(noseX, noseY, 20);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
